cebea699b6e86ec0fb60baeb922594521d469376
Matt Naruse <matnaruse@gmail.com>
2020-11-19 18:49:01 -0500

    [ZA1-D1D] Graph set up

--- Deliverables/D1_DataExploration_Modules/D1_D_GraphsVisualizations.py
+++ Deliverables/D1_DataExploration_Modules/D1_D_GraphsVisualizations.py
@@ -10,4 +10,16 @@ COMP309 Bike Theft Analysis
 Created on Wed Nov 11 13:06:20 2020
 """
 from D1_A_LoadDescribeData import BikeData
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
 
+class graph:
+    dataset = BikeData.get_dataset()
+    
+    print(dataset.columns)
+    
+    x = dataset ['X']
+    y = dataset ['Y']
+    plt.plot(x,y)
+    plt.show()
