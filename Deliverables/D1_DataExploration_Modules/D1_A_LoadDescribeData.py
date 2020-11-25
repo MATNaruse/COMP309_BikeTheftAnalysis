409f15df61791e83e8a9f616342c1b6f1828002e
Matt Naruse <matnaruse@gmail.com>
2020-11-25 15:49:32 -0500

    [REFACT-MN2] Cleaned up BikeData class

--- Deliverables/D1_DataExploration_Modules/D1_A_LoadDescribeData.py
+++ Deliverables/D1_DataExploration_Modules/D1_A_LoadDescribeData.py
@@ -16,33 +16,82 @@ from pathlib import Path
 
 pandas.set_option('display.expand_frame_repr', False)
 
-# Find Relative Paths 
-# (from ../COMP309_BikeTheftAnalysis/Deliverables/D1_DataExploration)
-path_dataset = os.path.join(Path(__file__).parents[2], "Dataset\Bicycle_Thefts.csv")
-path_dataset_meta = os.path.join(Path(__file__).parents[2], "Dataset\Bicycle_Thefts_Metadata.csv")
-
-# Load Data
-dataset = pandas.read_csv(path_dataset)
-dataset_meta = pandas.read_csv(path_dataset_meta)
-
-# Descriptions & Types
-dataset.dtypes
-#print(dataset_meta)
-
-# Ranges & Values
-dataset.describe()
-
 
 class BikeData:
     
-    @staticmethod
-    def get_dataset():
-        path_dataset = os.path.join(Path(__file__).parents[2], "Dataset\Bicycle_Thefts.csv")
-        dataset = pandas.read_csv(path_dataset)
-        return dataset
+    
+    FeatureColumns: [] = ["Primary_Offence", "Occurance_Time", "Location_Type",
+                          "Bike_Type", "Status", "Neighbourhood"]
     
     @staticmethod
-    def get_metadataset():
-        path_dataset_meta = os.path.join(Path(__file__).parents[2], "Dataset\Bicycle_Thefts_Metadata.csv")
+    def get_dataset() -> pandas.DataFrame:
+        """
+        DEPRECATED - Use get_raw_dataset()
+
+        Returns:
+        pandas.DataFrame : Created from 'Bicycle_Thefts.csv'
+
+        """
+        return BikeData.get_raw_dataset()
+    
+    @staticmethod
+    def get_metadataset() -> pandas.DataFrame:
+        """
+        DEPRECATED - Use get_raw_metadataset()
+
+        Returns:
+        pandas.DataFrame : Created from 'Bicycle_Thefts_Metadata.csv'
+
+        """
+        return BikeData.get_raw_metadataset()
+
+    @staticmethod
+    def get_raw_dataset() -> pandas.DataFrame:
+        """
+        Get DataFrame with Raw Data from 'Bicycle_Thefts.csv'
+
+        Returns
+        -------
+        dataset : pandas.DataFrame
+            Created from 'Bicycle_Thefts.csv', Unmodified
+
+        """
+        path_dataset = os.path.join(Path(__file__).parents[2],
+                                    "Dataset\Bicycle_Thefts.csv")
+        dataset = pandas.read_csv(path_dataset)
+        return dataset
+        
+    @staticmethod
+    def get_raw_metadataset() -> pandas.DataFrame:
+        """
+        Get DataFrame with Raw Data from 'Bicycle_Thefts_Metadata.csv'
+
+        Returns
+        -------
+        dataset_meta : pandas.DataFrame
+            Created from 'Bicycle_Thefts_Metadata.csv', Unmodified
+        """
+        path_dataset_meta = os.path.join(Path(__file__).parents[2], 
+                                         "Dataset\Bicycle_Thefts_Metadata.csv")
         dataset_meta = pandas.read_csv(path_dataset_meta)
         return dataset_meta
+    
+    @staticmethod
+    def get_trimmed_dataset() -> pandas.DataFrame:
+        """
+        Get DataFrame with all useful* columns ONLY
+        *Removed ["X","Y","FID", "Occurrence_Date", "City", "Bike_Speed", "Lat",
+                 "Long","Hood_ID"]
+
+        Returns
+        -------
+        dataset : pandas.DataFrame
+            DataFrame with Trimmed Columns from Raw Data
+
+        """
+        return BikeData.get_raw_dataset().drop(columns=["X","Y","FID", 
+                                                        "Occurrence_Date", 
+                                                        "City", "Bike_Speed", 
+                                                        "Lat", "Long", 
+                                                        "Hood_ID"])
+    
